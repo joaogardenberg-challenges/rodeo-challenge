e0d65569b014983e9a3871cdbf07526162d75e98
Johnny <joaogardenberg@gmail.com>
2023-03-30 18:32:38 +0200

    Import correctly

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from 'components/App'
 import GlobalStyles from './index.global.styled'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <GlobalStyles />
     <App />
