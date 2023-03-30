5f1ebbfe03963301792c5f4180a335af5fa60cc2
Johnny <joaogardenberg@gmail.com>
2023-03-30 05:03:40 +0200

    Created the CostItem component

---
+++ src/components/invoice/phase/CostItem.tsx
@@ -0,0 +1,42 @@
+import { useContext, useMemo } from 'react'
+import { CostItem as CostItemType } from 'types'
+import getCurrencySymbol from 'utils/getCurrencySymbol'
+import formatPrice from 'utils/formatPrice'
+import { getCostItemPrice } from 'utils/costItemPrice'
+import { InvoiceContext } from 'components/invoice/Invoice'
+
+interface CostItemProps {
+  costItem: CostItemType
+}
+
+export default function CostItem({ costItem }: CostItemProps) {
+  const { currency } = useContext(InvoiceContext)
+  const currencySymbol = getCurrencySymbol(currency)
+  const { billedPerUnit, name, price, tax, hours, units } = costItem
+  const quantity = billedPerUnit ? units || 1 : hours || 1
+  const totalPrice = getCostItemPrice(costItem)
+
+  const costUnit = useMemo(
+    () => `${billedPerUnit ? 'unit' : 'hour'}${quantity === 1 ? '' : 's'}`,
+    [billedPerUnit, quantity]
+  )
+
+  return (
+    <li className="cost-item">
+      <span hidden>Name: </span>
+      <span className="cost-item__name">{name}</span>
+      <span hidden>, Tax: </span>
+      <span className="cost-item__tax">{tax}%</span>
+      <span hidden>, Description: </span>
+      <span className="cost-item__description">
+        {currencySymbol}
+        {formatPrice(price)} x {quantity} {costUnit}
+      </span>
+      <span hidden>, Price: </span>
+      <span className="cost-item__price">
+        {currencySymbol}
+        {formatPrice(totalPrice)}
+      </span>
+    </li>
+  )
+}
