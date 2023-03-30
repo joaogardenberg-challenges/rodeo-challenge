1fbef6aeeb371b65e3cfeef49b6ccc44a3a150a1
Johnny <joaogardenberg@gmail.com>
2023-03-30 05:04:15 +0200

    Created the TotalPrice component

---
+++ src/components/invoice/TotalPrice.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default styled.section`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.2rem;
+  gap: 1rem;
+  margin: 2rem 1rem;
+
+  .total-title {
+    font-size: 2rem;
+    text-align: center;
+  }
+`
---
+++ src/components/invoice/TotalPrice.tsx
@@ -0,0 +1,88 @@
+import { useContext } from 'react'
+import formatPercent from 'utils/formatPercent'
+import formatPrice from 'utils/formatPrice'
+import getCurrencySymbol from 'utils/getCurrencySymbol'
+import {
+  getInvoicePrice,
+  getInvoicePriceWithFeeOrDiscountPercent,
+  getInvoicePriceWithTax
+} from 'utils/invoicePrice'
+import StyledTotalPrice from './TotalPrice.styled'
+import { InvoiceContext } from './Invoice'
+
+export default function TotalPrice() {
+  const invoice = useContext(InvoiceContext)
+  const { currency, feeOrDiscountPercent } = invoice
+  const currencySymbol = getCurrencySymbol(currency)
+  const subtotalPrice = getInvoicePrice(invoice)
+
+  const subtotalWithFeeOrDiscountPercent =
+    getInvoicePriceWithFeeOrDiscountPercent(invoice)
+
+  const subtotalWithTax = getInvoicePriceWithTax(invoice)
+  const feeOrDiscountAmount = subtotalWithFeeOrDiscountPercent - subtotalPrice
+  const taxAmount = subtotalWithTax - subtotalWithFeeOrDiscountPercent
+
+  const feeOrDiscountText = feeOrDiscountPercent
+    ? feeOrDiscountPercent > 0
+      ? 'fee'
+      : 'discount'
+    : null
+
+  const feeOrDiscountSymbol = feeOrDiscountPercent
+    ? feeOrDiscountPercent > 0
+      ? '+'
+      : '-'
+    : null
+
+  return (
+    <StyledTotalPrice className="total-price-section">
+      <h2 className="total-title">Total</h2>
+      <ol className="total-prices">
+        <li className="cost-item cost-item--total">
+          <span className="cost-item__name">Subtotal price</span>
+          <span hidden>: </span>
+          <span className="cost-item__tax" />
+          <span className="cost-item__description" />
+          <span className="cost-item__price">
+            {currencySymbol}
+            {formatPrice(subtotalPrice)}
+          </span>
+        </li>
+        {Boolean(feeOrDiscountPercent) && (
+          <li className="cost-item cost-item--total">
+            <span className="cost-item__name">
+              Subtotal price with {feeOrDiscountText} ({feeOrDiscountSymbol}{' '}
+              {formatPercent(Math.abs(feeOrDiscountPercent))}%)
+            </span>
+            <span hidden>: </span>
+            <span className="cost-item__tax" />
+            <span className="cost-item__description">
+              {feeOrDiscountSymbol} {currencySymbol}
+              {formatPrice(Math.abs(feeOrDiscountAmount))}
+            </span>
+            <span hidden> = </span>
+            <span className="cost-item__price">
+              {currencySymbol}
+              {formatPrice(subtotalWithFeeOrDiscountPercent)}
+            </span>
+          </li>
+        )}
+        <li className="cost-item cost-item--total">
+          <span className="cost-item__name">Total price with tax</span>
+          <span hidden>: </span>
+          <span className="cost-item__tax" />
+          <span className="cost-item__description">
+            + {currencySymbol}
+            {formatPrice(taxAmount)}
+          </span>
+          <span hidden> = </span>
+          <span className="cost-item__price">
+            {currencySymbol}
+            {formatPrice(subtotalWithTax)}
+          </span>
+        </li>
+      </ol>
+    </StyledTotalPrice>
+  )
+}
